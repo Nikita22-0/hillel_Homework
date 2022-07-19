d888a0ce84d897f82c417670f15cf6fc1e1313da
Nikita22-0 <znikitaz1@gmail.com>
2022-07-19 16:02:33 +0300

    Homework11

---
+++ lesson9/Homework11.java
@@ -0,0 +1,52 @@
+package com.hillel.paschenko.lesson9;
+
+import java.util.Scanner;
+
+public class Homework11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int m = getNumber(scanner);
+        int n = getNumber(scanner);
+        int[][] array = new int[m][n];
+        int[][] array2 = new int[n][m];
+        fillingArrays(array,array2);
+    }
+    static int getNumber(Scanner scanner) {
+        int temp;
+        while (true) {
+            if (scanner.hasNextInt()) {
+                temp = scanner.nextInt();
+                break;
+            } else {
+                System.out.println("Wrong data");
+                scanner.next();
+            }
+        }
+        return temp;
+    }
+    static void fillingArrays (int[][] array, int[][] array2){
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = (int) (Math.random() * 11);
+            }
+        }
+        for (int i = 0; i < array2.length; i++) {
+            for (int j = 0; j < array2[i].length; j++) {
+                array2[i][j] = (int) (Math.random() * 11);
+            }
+        }
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " \t");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (int i = 0; i < array2.length; i++) {
+            for (int j = 0; j < array2[i].length; j++) {
+                System.out.print(array2[i][j] + " \t");
+            }
+            System.out.println();
+        }
+    }
+}
