610f47280aa32c99b77c48ee989954414381155d
Nikita22-0 <znikitaz1@gmail.com>
2022-08-06 13:19:36 +0300

    Homework 17

---
+++ lesson14/Homework.java
@@ -0,0 +1,24 @@
+package com.hillel.paschenko.lesson14;
+
+public class Homework {
+
+    public static double average(int[][] array){
+        double sum = 0;
+        int a = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                sum += array[i][j];
+                a++;
+            }
+        }
+        return sum / a;
+    }
+    public static boolean square(int[][] array ){
+        for (int i = 0; i < array.length; i++) {
+            if(array.length != array[i].length){
+                return false;
+            }
+        }
+        return true;
+    }
+}
